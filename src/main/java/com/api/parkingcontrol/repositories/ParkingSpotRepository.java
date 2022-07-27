40170ab906307c3dfbde0e04069f24ee3c0b94cb
Marcus Vinicius <marcusviniferrer@gmail.com>
2022-07-27 10:24:38 -0300

    ParkingSpotModel Repository

---
+++ src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -0,0 +1,7 @@
+package com.api.parkingcontrol.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParkingSpotRepository extends JpaRepository<ParkingSpotRepository, UUID> {
+
+}
