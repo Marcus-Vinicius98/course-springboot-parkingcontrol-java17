b3be942cbef946900b742065480fdf5dfa68d17f
Marcus Vinicius <marcusviniferrer@gmail.com>
2022-07-27 10:34:03 -0300

    ParkingSpotController controller layer

--- src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
+++ src/main/java/com/api/parkingcontrol/repositories/ParkingSpotRepository.java
@@ -1,7 +1,11 @@
 package com.api.parkingcontrol.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.UUID;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface ParkingSpotRepository extends JpaRepository<ParkingSpotRepository, UUID> {
 
 }
