e13b9322076e021177ab52d6f240a4ce33c40866
Marcus Vinicius <marcusviniferrer@gmail.com>
2022-07-27 13:55:34 -0300

    Method save and some fixes

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -3,12 +3,17 @@ package com.api.parkingcontrol.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.api.parkingcontrol.model.ParkingSpotModel;
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
 
 @Service
 public class ParkingSpotService {
-	
+
 	@Autowired
-     ParkingSpotRepository parkingSpotRepository;
+    ParkingSpotRepository parkingSpotRepository;
+	
+	public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+		return parkingSpotRepository.save(parkingSpotModel);
+	}
 
 }
